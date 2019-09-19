df0fb934ddc73a871c10bd365bfa9ad6da38b272
Alexandria <alex.pugia816@hotmail.com>
2019-09-19 15:45:37 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@pirates = []
+
+    def initialize(info)
+        @name = info[:name]
+        @weight = info[:weight]
+        @height = info[:height]
+        @@pirates << self
+    end
+
+    def self.all
+        @@pirates
+    end
 end
